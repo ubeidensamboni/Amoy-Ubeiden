862c9d0cc7858b87b82e4cd7b05f687cbc45a6a4
Ubeiden Samboni <69818646+ubeidensamboni@users.noreply.github.com>
2025-06-17 14:06:36 +0100

    Update invitation.js

--- js/invitation.js
+++ js/invitation.js
@@ -1,1 +1,6 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+  const envelope = document.querySelector('.invitation-envelope');
+  envelope.addEventListener('click', () => {
+    envelope.classList.toggle('opened');
+  });
+});
